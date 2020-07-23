2e1308fdb9f4828397e53a986f87cf5de907ca0b
raha <elahe.dstn@gmail.com>
2020-07-23 12:59:47 +0430

    Add vowel substring

--- vowel-substring/main.go
+++ vowel-substring/main.go
@@ -4,19 +4,21 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"os"
+	"strconv"
 	"strings"
 )
+
+var vowels = []uint8{'a', 'e', 'i', 'o', 'u'}
 
 func findSubstring(s string, k int32) string {
 	var i int32
 	max := int32(0)
 	best := "Not found!"
-	min := int32(0)
-
-	vowels := []uint8{'a', 'e', 'i', 'o', 'u'}
+	maxNumOfVowels := int32(0)
 	counter := int32(0)
 
-	for i := 0; i < len(s); i++ {//O(k)
+	for i := 0; i < len(s); i++ {//O(n)
 		for _, v := range vowels {//5
 			if s[i] == v {
 				counter++
@@ -33,16 +35,16 @@ func findSubstring(s string, k int32) string {
 		return best
 	}
 
-	min = counter
+	maxNumOfVowels = counter
 
 	var v int32
-	for i = 0; i <= int32(len(s))-k; i++ { //O(n)
+	for i = 0; i <= int32(len(s))-k; i++ { //O(n - k)
 		if i == 0 {
 			v = countVowels(s[i : i+k])
 		}else {
 			v = nextSub(s[i-1 : i+k], v)
 		}
-		if v == min {
+		if v == maxNumOfVowels {
 			return s[i : i+k]
 		}
 
@@ -54,11 +56,10 @@ func findSubstring(s string, k int32) string {
 
 	fmt.Println(best)
 
-	return best //O(n * k * 5) + O(n)
+	return best //O(5 * n) + O(n - k) * 5
 }
 
 func countVowels(s string) int32 {
-	vowels := []uint8{'a', 'e', 'i', 'o', 'u'}
 	counter := int32(0)
 
 	for i := 0; i < len(s); i++ {//O(k)
@@ -74,8 +75,6 @@ func countVowels(s string) int32 {
 }
 
 func nextSub(s string, count int32) int32 {
-	vowels := []uint8{'a', 'e', 'i', 'o', 'u'}
-
 	for _, v := range vowels {//5
 		if s[0] == v {
 			count--
@@ -94,26 +93,26 @@ func nextSub(s string, count int32) int32 {
 }
 
 func main() {
-	//reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
-	//
-	//stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
-	//checkError(err)
-	//
-	//defer stdout.Close()
-	//
-	//writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
-	//
-	//s := readLine(reader)
-	//
-	//kTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
-	//checkError(err)
-	//k := int32(kTemp)
+	reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
 
-	findSubstring("azerdii", 5)
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
 
-	//fmt.Fprintf(writer, "%s\n", result)
-	//
-	//writer.Flush()
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+	s := readLine(reader)
+
+	kTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	k := int32(kTemp)
+
+	result := findSubstring("azerdii", 5)
+
+	fmt.Fprintf(writer, "%s\n", result)
+
+	writer.Flush()
 }
 
 func readLine(reader *bufio.Reader) string {
